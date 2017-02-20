18d43821faa9482f4f5f2258a62cee1deb600c07
hakan.karanfil <hakan.karanfil@ALS-YG39104-J04>
2017-02-20 09:32:08 +0200

    gfinish

---
+++ web_post/web_post/default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="web_post._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:DropDownList ID="DropDownList1" runat="server">
+        </asp:DropDownList>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ web_post/web_post/default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace web_post
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                DropDownList1.Items.Add("aytekin");
+                DropDownList1.Items.Add("çeyiz");
+                DropDownList1.Items.Add("hakan");
+                DropDownList1.Items.Add("bengisu");
+            }
+           
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            DropDownList1.Items.Add("atakan");
+        }
+    }
+}
