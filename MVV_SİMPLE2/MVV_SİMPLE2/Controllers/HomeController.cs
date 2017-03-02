6b4c3b0bd527918b7c54915f06d639f3b269aa37
hakan.karanfil <hakan.karanfil@ALS-YG39104-J04>
2017-03-02 11:01:18 +0200

    Finish

---
+++ MVV_SİMPLE2/MVV_SİMPLE2/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVV_SİMPLE2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // ActionResult Controller ile Wiew arasındaki ilişkileri  sağlayan metoddur.Olmak zorundadır.
+        public ActionResult Index()
+        {
+            ViewData["Mesasage1"] = "Bu bir View data mesajıdır.";
+            ViewBag.Message2="Bu bir ViewBag Mesajıdır.";
+            TempData["Message3"] = "Bu bir Tempdata mesajıdır";//Farklı vir view e veri göndermek için
+            return RedirectToAction("TempData");
+            //return View();
+        }
+        public ActionResult Tempdata()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVV_SİMPLE2/MVV_SİMPLE2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVV_SİMPLE2.MvcApplication" Language="C#" %>
