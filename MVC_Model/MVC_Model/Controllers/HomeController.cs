fce40910190d2a8e62030546472de9504bc7f017
hakan.karanfil <hakan.karanfil@ALS-YG39104-J04>
2017-03-02 11:31:08 +0200

    finish

---
+++ MVC_Model/MVC_Model/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using MVC_Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Model.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Student> Ogrenciler = new List<Student>();
+            Student ogrenci = new Student();
+            ogrenci.StudentID = 1;
+            ogrenci.Name =" Hakan";
+            ogrenci.Surname = "Karanfil";
+            ogrenci.Number = 123;
+            Ogrenciler.Add(ogrenci);
+            Student ogrenci2 = new Student();
+            ogrenci2.StudentID = 2;
+            ogrenci2.Name = " Emre";
+            ogrenci2.Surname = "Karanfil";
+            ogrenci.Number = 123;
+            Ogrenciler.Add(ogrenci2);
+            ViewBag.ogrenciList = Ogrenciler;
+            return View();
+        }
+    }
+}
---
+++ MVC_Model/MVC_Model/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Model.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Number { get; set; }
+    }
+}
