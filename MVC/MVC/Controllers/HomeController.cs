88736f107102962a7ae13f0299ba18fa260a8dd1
hakan.karanfil <hakan.karanfil@ALS-YG39104-J04>
2017-03-02 10:37:18 +0200

    finish

---
+++ MVC/MVC/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Merhaba Dünya";
+            return View();
+        }
+    }
+}
