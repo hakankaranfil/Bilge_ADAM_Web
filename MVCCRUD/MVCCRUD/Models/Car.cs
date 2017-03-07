c1598df768cd32111625eeebd026dd3d9cdc2410
Hakan <Hakan@DESKTOP-FMCAGTH>
2017-03-07 22:13:37 +0300

    UPDATE

---
+++ MVCCRUD/MVCCRUD/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCCRUD.Models
+{
+    public class Car
+    {
+        public int CarID { get; set; }
+        public string CarBrand { get; set; }
+        public string CarModel{ get; set; }
+        public string CarAge { get; set; }
+    }
+}
--- WebApplication1/WebApplication1/Models/Car.cs
+++ WebApplication1/WebApplication1/Models/Car.cs
@@ -2,9 +2,9 @@
 {
     public class Car
     {
-        public int ID { get; set; }
-        public string Brand { get; set; }
-        public string Model { get; set; }
-        public int Age { get; set; }
+        public int CarID { get; set; }
+        public string CarBrand { get; set; }
+        public string CarModel { get; set; }
+        public string  CarAge { get; set; }
     }
 }
