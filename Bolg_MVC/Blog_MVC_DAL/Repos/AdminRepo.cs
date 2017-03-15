df4879c9a1d03c47d274053ad2664c575004c948
Hakan <Hakan@DESKTOP-FMCAGTH>
2017-03-15 22:58:04 +0300

    update

---
+++ Bolg_MVC/Blog_MVC_DAL/Repos/AdminRepo.cs
@@ -0,0 +1,21 @@
+﻿using Blog_MVC_Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog_MVC_DAL.Repos
+{
+   public class AdminRepo
+    {
+        public static void Add(Admin adm)
+        {
+            using (BlogDBContext db = new BlogDBContext())
+            {
+                db.Admin.Add(adm);
+                db.SaveChanges();
+            }
+        }
+    }
+}
