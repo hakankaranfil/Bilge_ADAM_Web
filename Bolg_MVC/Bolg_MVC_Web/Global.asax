392f0b9626b326647e80294325b08faa22cd10c1
Hakan <Hakan@DESKTOP-FMCAGTH>
2017-03-23 17:56:45 +0300

    asd

--- Bolg_MVC/Bolg_MVC_Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bolg_MVC_Web.MvcApplication" Language="C#" %>
---
+++ E-Ticaret/E-Ticaret.WEB/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Ticaret.WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult BookDeatails()
+        {
+            return View();
+        }
+    }
+}
