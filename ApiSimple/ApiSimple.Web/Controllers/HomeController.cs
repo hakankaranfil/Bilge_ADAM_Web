a72d893e468e107fe71fbb789882881d03858463
hakan.karanfil <hakan.karanfil@ALS-YG39104-J04>
2017-04-03 11:56:03 +0200

    Finish

---
+++ ApiSimple/ApiSimple.Web/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ApiSimple.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public async Task<ActionResult> Index()
+        {
+            using (HttpClient client=new HttpClient())
+            {
+                var response =await client.GetAsync("http://localhost:49494/Api/Category");
+                var result = JsonConvert.DeserializeObject<List<Kategori>>(response.Content.ReadAsStringAsync().Result);
+                return View(result);
+            }
+           
+        }
+    }
+}
---
+++ ApiSimple/ApiSimple.Web/Kategori.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiSimple.Web
+{
+    public class Kategori
+    {
+        public int CategoriID { get; set; }
+        public string CategoriName { get; set; }
+    }
+}
